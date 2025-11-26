dc367aa460c5abb31f38a9c18849ac19bb1ad4fc
maxx-maxxing <maxxlazos93@gmail.com>
2025-11-26 09:14:08 -0700

    Added a lot of player scripting. Next to address only jumping on ground

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,15 +2,23 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
+
+    public Rigidbody rig; // Initialize Rigidbody when you want to control the physics of the GO
+                          // Back in inspector, c&d the rigidbody component to the Rig reference in the script component
+
+    public float jumpForce; // factor by which to increase magnitude
+    public float moveSpeed; // factor by which to increase magnitude
     void Update()
     {
-        
+        float x = Input.GetAxisRaw("Horizontal") * moveSpeed; // If input is D, x = 1; if A, x = -1
+        float z = Input.GetAxisRaw("Vertical") * moveSpeed; // If input is W, z = 1; if S, z = -1
+
+        rig.linearVelocity = new Vector3(x, rig.linearVelocity.y, z); // linearVelocity is being checked every frame.
+
+        if (Input.GetKeyDown(KeyCode.Space)) // When key is pressed down
+        {
+            rig.AddForce(Vector3.up * jumpForce, ForceMode.Impulse); // Add a Y force, with impulse-type force
+        }
     }
 }
