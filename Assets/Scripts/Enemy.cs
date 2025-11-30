9e32412475dcbb3d42a6d395f5ef28135db5a55b
maxx-maxxing <maxxlazos93@gmail.com>
2025-11-30 15:37:24 -0700

    Added Initial Movement Logic To Enemy

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -2,15 +2,30 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    public float speed;
+    public Vector3 moveOffset;
+    private Vector3 targetPos;
+    private Vector3 startPos;
+    
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        startPos = transform.position;
+        targetPos = startPos;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position = Vector3.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
+        if (transform.position == targetPos)
+        {
+            transform.position = startPos + moveOffset;
+        }
+        else
+        {
+            targetPos = startPos;
+        }
     }
 }
